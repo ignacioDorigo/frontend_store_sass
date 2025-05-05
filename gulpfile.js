fae0e1b1242d40eebee387582deaaba29d508bef
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2025-05-05 14:22:15 -0300

    creando script automizacion sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,17 @@
 import dartSass from "sass";
 import gulpSass from "gulp-sass";
+import { dest, series, src, watch } from "gulp";
 const sass = gulpSass(dartSass);
+
+export function compilarSass(done) {
+  src("./src/sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("./css"));
+  done();
+}
+
+export function watchFiles() {
+  watch("./src/sass/**/*.scss", compilarSass);
+}
+
+export default series(compilarSass, watchFiles);
